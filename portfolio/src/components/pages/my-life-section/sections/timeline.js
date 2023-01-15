4b5fbc390658052c18dabb4169117ce4dc9ab55d
Frederick Go <dn282145@dal.ca>
2023-01-15 11:06:21 -0400

    Changes to Timeline

--- portfolio/src/components/pages/my-life-section/sections/timeline.js
+++ portfolio/src/components/pages/my-life-section/sections/timeline.js
@@ -10,39 +10,28 @@ const Timeline = () => {
                 <tr data-aos="fade-up" data-aos-delay="500">
                     <td className='year'>2002</td>
                     <td>
-                        <span className=''>Born in <font size="+2">
-                            <a 
-                                href='https://en.wikipedia.org/wiki/Busan' 
-                                target="_blank" 
-                                rel="noreferrer" 
-                                className='location-link'
-                            >
-                                Busan
-                            </a></font>
-                        , 
-                        <a 
-                            href='https://en.wikipedia.org/wiki/South_Korea' 
-                            target="_blank" 
-                            rel="noreferrer" 
-                            className='location-link'
-                        > 
-                            South Korea
-                        </a>. </span>
+                        <span className=''>
+                            Born in&nbsp;
+                                <font size="+2">
+                                    <a href='https://en.wikipedia.org/wiki/Busan' target="_blank" rel="noreferrer" className='location-link'>
+                                        Busan
+                                    </a>
+                                </font>
+                            ,&nbsp;
+                            <a href='https://en.wikipedia.org/wiki/South_Korea' target="_blank" rel="noreferrer" className='location-link'>
+                                South Korea
+                            </a>
+                            , and moved to&nbsp;
+                            <font size="+2">
+                                <a href='https://en.wikipedia.org/wiki/Geoje' target="_blank" rel="noreferrer" className='location-link'>
+                                    Geoje
+                                </a>
+                            </font>
+                            , South Korea. 
+                        </span>
                     </td>
                 </tr>
-                <tr data-aos="fade-up" data-aos-delay="600">
-                    <td className='year'>2002</td>
-                    <td>
-                        <span className=''> Moved to <font size="+2">
-                            <a 
-                                href='https://en.wikipedia.org/wiki/Geoje' 
-                                target="_blank" rel="noreferrer" 
-                                className='location-link'
-                            >
-                                Geoje
-                            </a></font>, South Korea. </span>
-                    </td>
-                </tr>
+                
                 <tr data-aos="fade-up" data-aos-delay="700">
                     <td className='year'>2013</td>
                     <td>
@@ -52,20 +41,20 @@ const Timeline = () => {
                 <tr data-aos="fade-up" data-aos-delay="800">
                     <td className='year'>2020</td>
                     <td>
-                        <span className=''>Graduated high school: <font size="+2">
-                            <a 
-                                href='https://www.hgs.ns.ca/' 
-                                target="_blank" 
-                                rel="noreferrer" 
-                                className='location-link'
-                            >
-                                Halifax Grammar School
-                            </a></font> with High Honours </span><br></br>
-                        <span className='action-more'>
-                            High Honours given to students successfully finishing with 90% average or above. 
+                        <span className=''>
+                            Graduated high school:&nbsp;
+                                <font size="+2">
+                                    <a href='https://www.hgs.ns.ca/' target="_blank" rel="noreferrer" className='location-link'>
+                                        Halifax Grammar School
+                                    </a>
+                                </font>
+                            with High Honours 
                         </span><br></br>
                         <span className='action-more'>
-                            Received <font size="+2">International Baccalaureate</font> Full Diploma
+                            - High Honours given to students successfully finishing with 90% average or above. 
+                        </span><br></br>
+                        <span className='action-more'>
+                            - Received <font size="+2">International Baccalaureate</font> Full Diploma
                         </span>
                     </td>
                 </tr>
@@ -84,15 +73,12 @@ const Timeline = () => {
                         <span className='action-more-more'>
                             - Communication Technologies & <font size="+2">Cyber Security</font>
                         </span><br></br>
-                        <span className='action-more'>
-                            Received Sexton Scholar every semester: given to those who have achieved a GPA of 3.85 or higher. 
-                        </span>
                     </td>
                 </tr>
                 <tr data-aos="fade-up">
                     <td className='year'>2023</td>
                     <td>
-                        <span className=''> Planning on Graduating.</span><br></br>
+                        <span className=''> Planning on Graduating in May 2023.</span><br></br>
                         <span className='action-more'><font size="+2">Where next?</font></span>
                     </td>
                 </tr>
